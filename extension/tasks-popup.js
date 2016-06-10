10b497ebef04af302afd7354adc80269c3a402bb
Vishal Chaudhari <Vishal.Chaudhari@sungard.com>
2016-06-10 20:48:19 +0530

    new version

--- extension/tasks-popup.js
+++ extension/tasks-popup.js
@@ -1,129 +1,185 @@
-// check jquery
+    // check jquery
 (function(global){
     
-	if(!global.jQuery){
-			throw new Error("Jquery not defined");
-	}
-  var $ = global.jQuery;
-	
-	//options object optional
-	function init(options){
-		
-		console.log("initializing tasks-pop.js...");	
-	
-		$.get(chrome.extension.getURL('/tasks-popup.html'), function(data) {
-			//$(data).appendTo('body');
-			// Or if you're using jQuery 1.8+:
-
-			console.log(data);
-
-			$($.parseHTML(data)).appendTo('body');	
     
-      initHandlers();
-
-
-		});
-	}
-    
-    function initHandlers(){
-        $('#popup #min').click(function(){
-
-            
-              $('#popup .card').hide(500);
-             $('#popup #expand').show();
-            $('#popup #min').hide();
-            
-        });
-        
-         $('#popup #expand').click(function(){
-
-            
-              $('#popup .card').show(500);
-              $('#popup #min').show();
-             $('#popup #expand').hide();
-            
-        }); 
-        
-        $('#popup #hint').click(function(){
-           console.log("hinting..") ;
-        });
-
-        
-
+    if(!global.jQuery){
+        throw new Error("Jquery not defined");
     }
-	
-	function showWindow(){
-		$('.tasks-popup').show(500);
-	}
-	
-	function hideWindow(){
-		
-		$('.tasks-popup').hide(500);
-	}
-	
-	// arrayOfTasks
-	function addTasks(listOfTasks){
+    var $ = global.jQuery;
+    var ANIMATE_DELAY = 500; //500ms
+		var tasksArray = [];
+    var taskCounter = 0;
+        
+      //options object optional
+        function init(options){
 
-		
-		var div = $('.tasks-popup #tasks ul');
-		
-		listOfTasks.forEach(function(task){
-			div.append('<li>'+task+'</li>');
-			
-		});
-		
-		
-		
-		
-	}
-	
-	function clearTasks(){
-		$('.tasks-popup');
-		
-		
-	}
-	
-	
-	var TasksPopup = function TasksPopup(){};
-	
-	TasksPopup.prototype.init = init;
-	TasksPopup.prototype.addTasks = addTasks;
-	TasksPopup.prototype.showWindow = showWindow;
-	TasksPopup.prototype.hideWindow = hideWindow;
-	
-	
-	//export popup;
-	
-	global.tasks = new TasksPopup();
-	
-	
-	
-})(window);
+            console.log("initializing tasks-pop.js...");	
 
-(function test(global){
-	
-	var tasks = global.tasks;
-			
-	var listOfTasks = ['task1','task4','task3','task2'];
-	
-	tasks.init();
-	
-	global.setTimeout(function(){
-		tasks.showWindow();	
-	//	tasks.addTasks(listOfTasks);
-      
-//		global.setTimeout(function(){
-//			tasks.hideWindow();
-//
-//		},20000);//hide
-		
-	},2000)//show;
-	
-	
+            $.get(chrome.extension.getURL('/tasks-popup.html'), 
+                  function(data) {
 
-	
-	
-	
-})(window);
+//								  console.log(data);
+
+                    $($.parseHTML(data)).appendTo('body');	
+
+                    initHandlers();
+                    add2Tasks(nextTask(), nextTask());                   
+
+											
+                });
+        }
+				function refreshTasks(){            
+            removeFirst();
+           
+				};
+    
+        function removeFirst(){
+             $('#popup .first-card').slideUp(ANIMATE_DELAY, function() { 
+                 $(this).remove(); 
+                  moveSecond();
+                addTask(nextTask());
+             });
+            
+        };
+    
+        function moveSecond(){
+            $('#popup .second-card').removeClass('second-card').addClass('first-card');
+        };
+    
+        function addTask(task){            
+            var div = $('#popup').append($('<div class="card second-card"><p class="light">'+task+'</p></div>'));                        
+        };
+    
+        function nextTask(){
+            taskCounter = (taskCounter === tasksArray.length-1? 0 : ++taskCounter);
+            console.log(taskCounter);
+            console.log(tasksArray[taskCounter]);
+            return tasksArray[taskCounter];
+        }
+    
+        function initHandlers(){
+            $('#popup #min').click(function(){
+                hideWindow();
+            });
+
+            $('#popup #expand').click(function(){
+                showWindow();
+            }); 
+					
+					 	$('#popup #refresh').click(refreshTasks); 
+
+            $('#popup #hint').click(function(){
+               console.log("hinting..") ;
+            });
+
+        }
+
+        function restoreButtons(){
+            $('#popup #min').show();
+            $('#popup #expand').hide();
+            $('#popup #refresh').show();
+        }
+        function showWindow(){
+            var popup = $('#popup');
+            $('#popup').animate({
+                                height:'250px'
+                            }, ANIMATE_DELAY/2);
+            if(popup.is(':visible')){
+                $('#popup .card').show(ANIMATE_DELAY, function(){
+                    restoreButtons();
+                });
+            }else{
+                popup.show(ANIMATE_DELAY,function(){
+                    restoreButtons();
+                });
+            }
+        }
+
+        function hideWindow(){
+            $('#popup .card').hide(ANIMATE_DELAY, function(){
+                $('#popup #expand').show();
+                $('#popup #min').hide();
+                $('#popup #refresh').hide();
+                $('#popup').animate({
+                                height:'20px'
+                            }, ANIMATE_DELAY/2);
+            });
+        }
+
+        function addToTasks(task){            
+           Array.prototype.push.apply(tasksArray, task); 
+			     //tasksArray.push(task);            
+        }
+	   
+        function add2Tasks(task1, task2){
+
+            var div = $('#popup');
+            if(task1){
+                var card1Div = $('<div class="card first-card"><p class="light">'+task1+'</p></div>');
+                div.append(card1Div);
+            }
+            
+            if(task2){
+                var card2Div = $('<div class="card second-card"><p class="light">'+task2+'</p></div>');
+                div.append(card2Div)    
+            }
+        }
+		
+
+      function clearTasks(){
+       	tasksArray = [];
+        taskCounter = 0;
+      }
+
+
+      var TasksPopup = function TasksPopup(){};
+
+      TasksPopup.prototype.init = init;
+      TasksPopup.prototype.addToTasks = addToTasks;
+			TasksPopup.prototype.refresh = refreshTasks;
+			TasksPopup.prototype.clearTasks = clearTasks;
+      TasksPopup.prototype.showWindow = showWindow;
+      TasksPopup.prototype.hideWindow = hideWindow;
+
+
+      //export popup;
+
+      global.tasks = new TasksPopup();
+
+
+
+    })(window);
+
+    (function test(global){
+
+      var tasks = global.tasks;
+
+      var listOfTasks = ['task1','task4','task3','task2'];
+        tasks.addToTasks(listOfTasks);
+        tasks.init();
+
+        global.setTimeout(function(){
+        
+            tasks.showWindow();	
+            
+          
+        
+        
+//      	tasks.add2Tasks('This is task1. Add an activity.', 
+//												'This is taks2. Create a transition from 1 to 2');
+
+    //		global.setTimeout(function(){
+    //			tasks.hideWindow();
+    //
+    //		},20000);//hide
+
+      },2000)//show;
+
+
+
+
+
+
+    })(window);
 
 
