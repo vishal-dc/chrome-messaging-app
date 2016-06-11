0b31febb90a6d6758c7d18ee0f1c10c3c79c9c01
Vishal Chaudhari <Vishal.Chaudhari@sungard.com>
2016-06-11 13:39:43 +0530

    Animations OK

--- extension/tasks-popup.js
+++ extension/tasks-popup.js
@@ -6,10 +6,14 @@
         throw new Error("Jquery not defined");
     }
     var $ = global.jQuery;
-    var ANIMATE_DELAY = 500; //500ms
+    /** Constants */
+    var ANIMATE_DELAY = 500, //500ms
+        POPUP_MIN_HEIGHT = '20px',
+        POPUP_MAX_HEIGHT = '250px';
+    
 		var tasksArray = [];
     var taskCounter = 0;
-        
+    
       //options object optional
         function init(options){
 
@@ -29,26 +33,28 @@
                 });
         }
 				function refreshTasks(){            
-            removeFirst();
+            $('#popup .first-card').css("background", "green").fadeOut(ANIMATE_DELAY, function() {
+                var height = $(this).height();  
+                var firstCard = this;                
+                  
+                 $('#popup .second-card').animate({
+                     bottom: "+="+height 
+                 }, ANIMATE_DELAY, 
+                      function(){
+                        firstCard.remove();
+                     
+                        $( this ).switchClass( "second-card", "first-card", ANIMATE_DELAY);         
+                    }
+                 );// end animate                 
+                  
+             });
+            var secondCard = $('<div class="card opaque-0"><p class="light">'+nextTask().text+'</p></div>');
+            $('#popup').append(secondCard);
+            secondCard.switchClass("opaque-0",  "second-card", 4*ANIMATE_DELAY); 
            
 				};
     
-        function removeFirst(){
-             $('#popup .first-card').slideUp(ANIMATE_DELAY, function() { 
-                 $(this).remove(); 
-                  moveSecond();
-                addTask(nextTask());
-             });
-            
-        };
-    
-        function moveSecond(){
-            $('#popup .second-card').removeClass('second-card').addClass('first-card');
-        };
-    
-        function addTask(task){            
-            var div = $('#popup').append($('<div class="card second-card"><p class="light">'+task+'</p></div>'));                        
-        };
+        
     
         function nextTask(){
             taskCounter = (taskCounter === tasksArray.length-1? 0 : ++taskCounter);
@@ -82,7 +88,7 @@
         function showWindow(){
             var popup = $('#popup');
             $('#popup').animate({
-                                height:'250px'
+                                height: POPUP_MAX_HEIGHT
                             }, ANIMATE_DELAY/2);
             if(popup.is(':visible')){
                 $('#popup .card').show(ANIMATE_DELAY, function(){
@@ -101,7 +107,7 @@
                 $('#popup #min').hide();
                 $('#popup #refresh').hide();
                 $('#popup').animate({
-                                height:'20px'
+                                height: POPUP_MIN_HEIGHT
                             }, ANIMATE_DELAY/2);
             });
         }
@@ -115,12 +121,12 @@
 
             var div = $('#popup');
             if(task1){
-                var card1Div = $('<div class="card first-card"><p class="light">'+task1+'</p></div>');
+                var card1Div = $('<div class="card first-card"><p class="light">'+task1.text+'</p></div>');
                 div.append(card1Div);
             }
             
             if(task2){
-                var card2Div = $('<div class="card second-card"><p class="light">'+task2+'</p></div>');
+                var card2Div = $('<div class="card second-card"><p class="light">'+task2.text+'</p></div>');
                 div.append(card2Div)    
             }
         }
@@ -154,32 +160,34 @@
 
       var tasks = global.tasks;
 
-      var listOfTasks = ['task1','task4','task3','task2'];
+        var task1 = {
+            id: 1,
+            text: 'Create an activity to do this!!',
+            hint: '1.gif'       
+        };
+        var task2 = {
+            id: 2,
+            text: 'Name it as activity 1!!',
+            hint: '2.gif'       
+        };        
+        
+        var task3 = {
+            id: 3,
+            text: 'Name it as activity 2!!',
+            hint: '2.gif'       
+        };      
+        
+        var listOfTasks = [task1, task2, task3];
         tasks.addToTasks(listOfTasks);
-        tasks.init();
+        tasks.init(); // display init
 
         global.setTimeout(function(){
-        
+
             tasks.showWindow();	
-            
-          
-        
-        
-//      	tasks.add2Tasks('This is task1. Add an activity.', 
-//												'This is taks2. Create a transition from 1 to 2');
 
-    //		global.setTimeout(function(){
-    //			tasks.hideWindow();
-    //
-    //		},20000);//hide
-
-      },2000)//show;
+        },2000)//show;
 
 
-
-
-
-
-    })(window);
+    })(window);// top IIFE
 
 
