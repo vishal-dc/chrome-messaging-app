85e46b51f38f332cb726ef2638381c71425a59fa
Vishal Chaudhari <Vishal.Chaudhari@sungard.com>
2016-06-14 08:39:43 +0530

    WIP verify

--- extension/tasks-popup.js
+++ extension/tasks-popup.js
@@ -88,7 +88,7 @@
 				};
     
         function showHint(){
-            var task = getCurrentTask();     
+            var task = currentTask();     
             $('#hintWindow img').attr('src', getGif((task.hint.path || 'dummy.gif')));    
             $('#hintWindowContainer').fadeIn(ANIMATE_DELAY/2);
             
@@ -100,13 +100,22 @@
         
        
     
-        function getCurrentTask(){
+        function currentTask(){
             return tasksArray[taskCounter];
         }
     
         function nextTask(){
-            taskCounter = (taskCounter === tasksArray.length-1? 0 : ++taskCounter);
-            return tasksArray[taskCounter];
+            if(taskCounter === tasksArray.length-1){
+                taskCounter = 0;
+                return {
+                    text: 'You are almost there... Last one FTW!!! '
+                };
+            }else{
+                return tasksArray[++taskCounter];
+            }
+        
+//            taskCounter = (taskCounter === tasksArray.length-1? 0 : ++taskCounter);
+//            return tasksArray[taskCounter];
         };
     
         function initHandlers(){
@@ -214,11 +223,11 @@
                 var stars = $('#completionWindow .stars-container i')
                     .switchClass('fa-star', 'fa-star-o', ANIMATE_DELAY/2).delay(ANIMATE_DELAY);
                 
-                stars.eq(0).delay(ANIMATE_DELAY*2).switchClass('fa-star-o', 'fa-star', ANIMATE_DELAY,'swing', function(){
+                stars.eq(0).delay(ANIMATE_DELAY*1).switchClass('fa-star-o', 'fa-star', ANIMATE_DELAY,'swing', function(){
                     if(1 < level){
-                        stars.eq(1).delay(ANIMATE_DELAY*3).switchClass('fa-star-o', 'fa-star', ANIMATE_DELAY,'swing', function(){
+                        stars.eq(1).delay(ANIMATE_DELAY*2).switchClass('fa-star-o', 'fa-star', ANIMATE_DELAY,'swing', function(){
                             if(2 < level){
-                                stars.eq(2).delay(ANIMATE_DELAY*4).switchClass('fa-star-o', 'fa-star', ANIMATE_DELAY,'swing', function(){
+                                stars.eq(2).delay(ANIMATE_DELAY*3).switchClass('fa-star-o', 'fa-star', ANIMATE_DELAY,'swing', function(){
                                     animateImage();    
                                 });
                                 
@@ -230,23 +239,20 @@
                         animateImage();
                     }
                 });
-                
-                
-//                .filter(function(index){ 
-//                        return index < level;
-//                    }).switchClass('fa-star-o', 'fa-star', ANIMATE_DELAY*8);
-                
-            });
         };
-    
+
+        function currentTaskIndex(){
+            return taskCounter;
+        }
         function hideCompletion(){     
-            var task = getCurrentTask();
-            
             $('#completionWindow').hide("explode", { pieces: 64 }, ANIMATE_DELAY*2);
             
             $('#completionContainer').fadeOut(ANIMATE_DELAY/2);
             
-        };    
+        };  
+        function addLessonTitle(title){
+            $('#popup #lesson .title').text(title);
+        }
       //export popup;
 
       global.tasks = {
@@ -256,7 +262,10 @@
           clearTasks : clearTasks,
           showWindow : showWindow,
           hideWindow : hideWindow,
-          showCompletionPopup: showCompletionPopup
+          showCompletionPopup: showCompletionPopup,
+          currentTask : currentTask,
+          addLessonTitle: addLessonTitle,
+          currentTaskIndex : currentTaskIndex
       };
     
         function getGame(){
@@ -267,10 +276,112 @@
 
 (function game(global){
     var level = 1;
+    var lesson;
+    var intervalId;
     
-    function init(){
+    function init(lesson){
+        addLesson(lesson);
+    };
+    
+    function stopVerify(){
+        window.clearInterval(intervalId);
+    }
+    
+    function startLesson(lesson){
+        var tasks = global.tasks;
+        //tasks.hideWindow();
+        tasks.clearTasks();
+        tasks.addLessonTitle(lesson.title);
+        tasks.addToTasks(lesson.steps);
+        tasks.init();
+        setTimeout(function(){
+            tasks.showWindow(); // for tasks popups steps in less = tasks;
+            startVerify();
+        },1000);
+        
+        
+        // start verification
+       
+    }
+    function startVerify(){
+        var step = tasks.currentTask();
+        var length = lesson.steps.length;
+        intervalId = setInterval(function(){
+            var counter = 0;
+            console.log("interval Id: ", intervalId);
+            console.log('Checking....');
+            if(tasks.currentTaskIndex < length){
+                 if(step.verify(step.params, counter)){
+                    counter = 0;
+                    refreshTasks();
+                }else{
+                    counter++;
+                }
+            }else{
+                // list of steps end cancel interval
+                clearInterval(intervalId);
+                // calculate results/score
+                var result = completionResults();
+                tasks.showCompletionPopup(result);
+            }
+           
+        }, 1000);
         
     };
+    
+    function doVerify(step, counter){
+        
+    };
+    
+    function addLesson(lesson){
+        lesson = lesson;
+        lesson =  {
+            id: 1,
+            title: 'Mastering the workflow execution!',
+            steps: [
+                {
+                    text: 'Open Personal Worklist',
+                    verify: 'xpathVerifier',
+                    params: [
+                        'worklistXpath'
+                    ], 
+                    hint: {
+                        path: '1.gif',
+                        used: false,
+                        points: 100
+                    },
+                    complete : false
+                },
+                {
+                    text: 'Start Gamer Process',
+                    verify: 'xpathVerifier',
+                    params: [
+                        'startedProcessXpath'
+                    ], 
+                    hint: {
+                        path: '1.gif',
+                        used: false,
+                        points: 100
+                    },
+                    complete : false
+                },
+                {
+                    text: 'Open Activiy Process',
+                    verify: 'xpathVerifier',
+                    params: [
+                        'startedProcessXpath'
+                    ], 
+                    hint: {
+                        path: '1.gif',
+                        used: false,
+                        points: 100
+                    },
+                    complete : false
+                }
+            ],
+            
+        };
+    }
     
     function completionResults(task){
         level = ++level === 4 ? (level =1) : level;
@@ -294,6 +405,14 @@
             image : image
         };
     };
+       //return boolean
+    function xpathVerifier(xpath, counter)   {
+        // verify xpath presence in dom
+        console.log("Verifying xpath");
+        counter===5 ? true: false;
+        
+    };
+    
     global.game = {
         init : init,
         completionResults : completionResults       
@@ -302,52 +421,10 @@
     
 }(window));
 
-    (function test(global){
-
-      var tasks = global.tasks;
-
-        var task1 = {
-            id: 1,
-            text: 'Create an activity to do this!!',
-            hint: {
-                path: '1.gif',
-                used: false
-            }
-        };
-        var task2 = {
-            id: 2,
-            text: 'Name it as activity 1!!',
-            steps: [
-                {
-                    
-                }
-            ],
-            hint: {
-                path: '2.gif',
-                used: false
-            }
-        };        
-        
-        var task3 = {
-            id: 3,
-            text: 'Name it as activity 2!!',
-            hint: {
-                path: '1.gif',
-                used: false
-            }
-        };      
-        
-        var listOfTasks = [task1, task2, task3];
-        tasks.addToTasks(listOfTasks);
-        tasks.init(); // display init
-
-        global.setTimeout(function(){
-
-            tasks.showWindow();	
-
-        },2000)//show;
 
 
-    })(window);// top IIFE
+(function test(global){
+    global.game.init();
+})(window);// top IIFE
 
 
